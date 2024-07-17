1897b27c29f51365d8ee49822cac269d425178d5
Akshay946187 <aki748434@gmail.com>
2024-07-17 10:16:06 +0530

    making privite and auth route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,17 +4,42 @@ import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom'
 import Auth from './pages/auth'
 import Chat from './pages/chat'
 import Profile from './pages/profile'
+import { useSelector } from 'react-redux'
 
+
+const PriviateRoute = ({children})=>{
+  const {userInfo} = useSelector((state)=>state.auth)
+  const isAuthenticated =!! userInfo
+  return isAuthenticated ? children:<Navigate to="/auth"/>
+}
+
+const AuthRoute = ({children})=>{
+  const {userInfo} = useSelector((state)=>state.auth)
+  const isAuthenticated =!! userInfo
+  return isAuthenticated ?<Navigate to="/chat"/>: children
+}
 
 function App() {
  
-
+  
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/auth' element={<Auth/>}/>
-      <Route path='/chat' element={<Chat/>}/>
-      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/auth' element={
+        <AuthRoute>
+          <Auth/>
+        </AuthRoute>
+      }/>
+      <Route path='/chat' element={
+        <PriviateRoute>
+          <Chat/>
+        </PriviateRoute>
+      }/>
+      <Route path='/profile' element={
+        <PriviateRoute>
+          <Profile/>
+        </PriviateRoute>
+      }/>
       <Route path='*' element={<Navigate to='/auth'/>}/>
     </Routes>
     </BrowserRouter>
